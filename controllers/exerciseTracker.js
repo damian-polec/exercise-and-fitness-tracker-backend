dfca73b3df16efc480dc6ff14cd79d0c2bb6c59e
Damian Polec <damian.polec11@gmail.com>
2019-04-13 22:42:58 +0200

    added endpoints for saving and geting notes"

--- controllers/exerciseTracker.js
+++ controllers/exerciseTracker.js
@@ -1,4 +1,5 @@
 const Data = require('../models/data');
+const Exercise = require('../models/exercise');
 
 exports.getData = async (req, res, next) => {
   try {
@@ -14,5 +15,36 @@ exports.getData = async (req, res, next) => {
   } catch (error) {
     console.log(error);
   }
-  
+}
+
+exports.addNote = async (req, res, next) => {
+  try {
+    const dayId = req.body.dayId;
+    const noteData = req.body.noteData.filter(note => {
+      if(!note._id) {
+        return note;
+      }
+    });
+    const day = await Data.findOne({ _id: dayId });
+    const exercises = noteData.map(exercise => {
+      const exerciseObj = new Exercise({
+        exerciseType: exercise.exerciseType,
+        time: exercise.time,
+        dayId: dayId
+      })
+      return exerciseObj;
+    })
+    await Exercise.insertMany(exercises)
+    day.exercises = day.exercises.concat(exercises);
+    await day.save();
+    res.json({message: 'Note added'});
+  } catch(err) {
+    res.json(err);
+  }
+}
+
+exports.getNote = async ( req, res, next ) => {
+  const noteId = req.params.noteId;
+  const note = await Data.findOne({_id: noteId}).populate('exercises');
+  res.json({data: note});
 }
--- routes/tracker.js
+++ routes/tracker.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.post('/getData', trackerController.getData);
 
+router.post('/addNote', trackerController.addNote);
+
+router.get('/getNoteData/:noteId', trackerController.getNote);
+
 module.exports = router;
